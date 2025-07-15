b97569ae68b1376853f45ac8bcbc98658aff4668
Renu-haaa <renuhasreedharan@gmail.com>
2025-07-15 14:26:54 +0800

    Fix typo: rpute → route

--- render-flask-api.py
+++ render-flask-api.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.rpute("/")
+@app.route("/")
 def home():
     return "Hello from Render!"
+
+# For deploying to render, this section should not exist.
+# render uses gunicorn to run the app!
+# Just comment it out # or remove it !
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
