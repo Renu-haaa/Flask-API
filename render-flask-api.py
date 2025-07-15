5b98ddc2cd6503eeb6dfe641dd799fc63f79399d
Renu-haaa <renuhasreedharan@gmail.com>
2025-07-15 16:42:55 +0800

    Update home route with API key protection

--- render-flask-api.py
+++ render-flask-api.py
@@ -1,14 +1,23 @@
-from flask import Flask 
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env
+
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
+def require_api_key(func):
+    def wrapper(*args, **kwargs):
+        key = request.headers.get('x-api-key')
+        if key != API_KEY:
+            abort(401, description="Unauthorized")
+        return func(*args, **kwargs)
+    wrapper.__name__ = func.__name__
+    return wrapper
+
 @app.route("/")
+@require_api_key
 def home():
-    return "Hello from Render!"
-
-# For deploying to render, this section should not exist.
-# render uses gunicorn to run the app!
-# Just comment it out # or remove it !
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    return "Welcome with a valid API key!"
